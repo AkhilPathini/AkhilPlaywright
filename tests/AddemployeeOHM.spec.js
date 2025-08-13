11a608265d384d5cbfd0d56e461cf0a7cb0d532e
AkhilPathini <pathiniakhil1@gmail.com>
2025-08-13 18:42:10 +0530

    commit framework1

--- tests/AddemployeeOHM.spec.js
+++ tests/AddemployeeOHM.spec.js
@@ -40,18 +40,3 @@ test('Addemployee', async ({ page }) => {
 });
 
     
-//  import {test,expect } from '@playwright/test';
-//  test('Addemployee',async({page})=>{
-//     await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
-//     await page.locator("input[name='username']").fill("Admin")
-//     await page.locator("input[type='password']").fill("admin123")
-//     await page.locator("button[type='submit']").click()
-//     await expect(page).toHaveURL("https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index")
-    
-//     await page.locator("(//li[@class='oxd-main-menu-item-wrapper']//a)[2]").click()
-//     await page.locator("//a[normalize-space(text())='Add Employee']").click()
-//     await page.locator("//input[@placeholder='First Name']").fill("Krishna") 
-//     await page.locator("//input[@placeholder='Last Name']").fill("Reddy")
-//     await page.locator("//button[@type='submit']").click()
-
-// });
